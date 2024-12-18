70d8faa22007f6212bc86a81fc637a4043debe78
Shuvadeep Mandal <shuvadeepmandal5@gmail.com>
2024-12-18 15:12:25 +0530

    code box removed

--- components/shared/CodeBox.tsx
+++ components/shared/CodeBox.tsx
@@ -1,24 +1,32 @@
 "use client";
+
 import { useTheme } from "next-themes";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import {
   atomDark,
   oneLight,
 } from "react-syntax-highlighter/dist/esm/styles/prism";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Copy, Check } from "lucide-react";
 import { Button } from "../ui/button";
 
 const CodeBox = ({ code }: { code: string }) => {
   const { theme } = useTheme();
+  const [syntaxStyle, setSyntaxStyle] = useState(oneLight); // Default to light theme
   const [copied, setCopied] = useState(false);
 
-  const syntaxStyle =
-    theme === "dark" ||
-    (theme === "system" &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches)
-      ? atomDark
-      : oneLight;
+  // Dynamically determine the theme
+  useEffect(() => {
+    if (
+      theme === "dark" ||
+      (theme === "system" &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      setSyntaxStyle(atomDark);
+    } else {
+      setSyntaxStyle(oneLight);
+    }
+  }, [theme]);
 
   const handleCopy = () => {
     navigator.clipboard.writeText(code).then(() => {
@@ -28,8 +36,16 @@ const CodeBox = ({ code }: { code: string }) => {
   };
 
   return (
-    <div className="overflow-x-auto relative">
-      <SyntaxHighlighter language="cpp" style={syntaxStyle}>
+    <div className="relative w-full">
+      <SyntaxHighlighter
+        language="cpp"
+        style={syntaxStyle}
+        customStyle={{
+          borderRadius: "8px",
+          padding: "10px",
+          overflowX: "auto",
+        }}
+      >
         {code}
       </SyntaxHighlighter>
       <Button
@@ -37,7 +53,7 @@ const CodeBox = ({ code }: { code: string }) => {
         size={"icon"}
         variant={"secondary"}
         disabled={copied}
-        className="absolute top-4 right-2 z-10"
+        className="absolute top-4 right-4 z-10"
       >
         {copied ? <Check /> : <Copy />}
       </Button>
